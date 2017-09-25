acba648de786422acf4bc1e88bd0f5d289121bc5
Robert Puls <rpu1s@sbcglobal.net>
2017-09-25 10:51:23 -0600

    styling

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,6 +3,6 @@ import DS from 'ember-data';
 export default DS.RESTAdapter.extend({
   user: Ember.inject.service(),
   url: Ember.computed.alias("user.address"),
-  host: "http://10.6.67.80:3000"
+  host: "http://10.2.6.45:3000"
   // host: `http:${this.get("url")}`
 });
